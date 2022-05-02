dbfecd0c18dee64fc848e2e7e44bfd61452a0913
Pankorn Chantarod <pankorn.ch@gmail.com>
2022-05-02 23:36:21 +0700

    fix speech voice index & fix advice ul

--- src/components/ButtonToSpeech.tsx
+++ src/components/ButtonToSpeech.tsx
@@ -1,5 +1,5 @@
 import { VolumeUpIcon } from "@heroicons/react/outline"
-import { FC } from "react"
+import { FC, useMemo } from "react"
 import { useSpeechSynthesis } from "react-speech-kit"
 import NoSSR from "./NoSSR"
 
@@ -10,14 +10,17 @@ interface Props {
 
 const ButtonToSpeech: FC<Props> = ({ text, children }) => {
 	const { speak } = useSpeechSynthesis()
+
+	const getVoice = (lang: string) => {
+		const voices = window.speechSynthesis.getVoices()
+		const voice = voices.find((v) => v.lang === lang)
+		return voice
+	}
+
+
 	return (
 		<div className="flex items-start gap-x-2">
-			<button
-				className="p-2"
-				onClick={() =>
-					speak({ text, voice: window.speechSynthesis.getVoices()[16] })
-				}
-			>
+			<button className="p-2" onClick={() => speak({ text, voice: getVoice("th-TH") })}>
 				<VolumeUpIcon className="w-6 h-6 text-black" />
 			</button>
 			{children}
